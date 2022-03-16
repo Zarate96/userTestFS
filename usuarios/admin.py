67b69cfefd52802ade6a6cac553d835354d6624e
Your Name <you@example.com>
2022-03-16 14:58:14 -0600

    multiply changes

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MyUser, Cliente, Transportista, Contacto, DatosFiscales, Unidades, Encierro, Telefonias
+from .models import MyUser, Cliente, Transportista, Contacto, DatosFiscales, Unidades, Encierro, Telefoniasdas
 
 admin.site.register(MyUser)
 admin.site.register(Cliente)
