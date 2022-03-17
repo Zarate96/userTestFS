dc6719fb59fef6dfeb670b6a4dce544abc4a3609
Your Name <you@example.com>
2022-03-16 18:58:15 -0600

    multiply changes

--- fleteSeguro/settings/production.py
+++ fleteSeguro/settings/production.py
@@ -1,7 +1,7 @@
 import os
 import dj_database_url
 from decouple import config
-from .base import *
+from fleteSeguro.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
