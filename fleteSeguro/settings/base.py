7b890d8f5d31ac9107169c9b74116565f85de3cc
Your Name <you@example.com>
2022-04-18 19:13:24 -0500

    updated fletes app

--- fleteSeguro/settings/base.py
+++ fleteSeguro/settings/base.py
@@ -99,8 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-#TIME_ZONE = 'America/Mexico_City'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
+#TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
