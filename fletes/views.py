54b915552dc9b8c2ba82aa15e528535e2f7b83af
Your Name <you@example.com>
2022-04-26 00:18:40 -0500

    add configs

--- fletes/views.py
+++ fletes/views.py
@@ -894,7 +894,7 @@ def PagarCotizacion(request, slug):
             return HttpResponseRedirect(url_link)
         except conekta.ConektaError as e:
             print(e.message)
-            messages.success(request, f'No se pudo generar la orden!!')
+            messages.success(request, f'No se pudo generar la orden!!{e.message}')
             return HttpResponseRedirect(reverse('fletes:checkout', kwargs={'slug': cotizacion.slug}))
 
 def PagoConfirmado(request):
