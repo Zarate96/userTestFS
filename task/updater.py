2500d87d5e8a882ad278bd41b605e54b325b1477
Your Name <you@example.com>
2022-04-29 14:14:02 -0500

    Config conekta, add task folder and profile autocomplete function

--- task/updater.py
+++ task/updater.py
@@ -4,7 +4,6 @@ from . import tasks
 
 def start():
     scheduler = BackgroundScheduler()
-    #add comment
     #scheduler.add_job(tasks.checkLinkStatus, 'interval', minutes=1)
     #scheduler.add_job(selenium.get_hcRutasInter, 'cron', day_of_week='0-6', hour='7')
     scheduler.start()
