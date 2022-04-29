48ebc27f63b9198e557bd7260ee9600ba4a31059
Your Name <you@example.com>
2022-04-29 14:22:37 -0500

    Config conekta, add task folder and profile autocomplete function

--- fleteSeguro/wsgi.py
+++ fleteSeguro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fleteSeguro.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fleteSeguro.settings.local')
 
 application = get_wsgi_application()
