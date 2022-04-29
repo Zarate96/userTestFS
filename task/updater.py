3234648575ade3bafb1fedf1a448faf7284846bd
Your Name <you@example.com>
2022-04-29 17:32:34 -0500

    add task interval 60

--- task/updater.py
+++ task/updater.py
@@ -4,6 +4,7 @@ from . import tasks
 
 def start():
     scheduler = BackgroundScheduler()
-    #scheduler.add_job(tasks.checkLinkStatus, 'interval', minutes=1)
+    #scheduler = AsyncIOScheduler(timezone="'America/Mexico_City'")
+    scheduler.add_job(tasks.checkLinkStatus, 'interval', minutes=60)
     #scheduler.add_job(selenium.get_hcRutasInter, 'cron', day_of_week='0-6', hour='7')
     scheduler.start()
